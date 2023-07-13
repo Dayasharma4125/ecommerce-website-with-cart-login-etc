5efd56fac63b7d4fdced7ac8148678221e0fdfa5
Dayasharma4125 <dayasharma4125@gmail.com>
2023-07-13 20:25:35 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      "/cart":"https://loginappbackend-hs2o.onrender.com",
-      "/login":"https://loginappbackend-hs2o.onrender.com",
-      "/register":"https://loginappbackend-hs2o.onrender.com"
+  server: {
+    proxy: {
+      "/cart": "https://loginappbackend-hs2o.onrender.com",
+      // "/login":"https://loginappbackend-hs2o.onrender.com",
+      "/register": "https://loginappbackend-hs2o.onrender.com",
+      "/login": {
+        target: 'https://loginappbackend-hs2o.onrender.com',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      }
     }
   },
-  base:"/webapp1/",
+  base: "/webapp1/",
   plugins: [react()],
 })
